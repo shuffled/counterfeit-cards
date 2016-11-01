e417fd199bfc3d65eaa41e23ef75c60e390dba2b
Stuart P. Bentley <stuart@testtrack4.com>
2016-11-01 14:18:28 +0000

    Refactor brand to be locals-wide

--- render.js
+++ render.js
@@ -6,4 +6,21 @@ let pug = require('pug');
 
 let renderer = pug.compile(fs.readFileSync('index.pug'));
 let locals = yaml.load(fs.readFileSync('locals.yaml'));
+
+if (!locals.brand) {
+  locals.brand = {};
+}
+if (!locals.brand.name) {
+  locals.brand.name = 'Counterfeit Cards';
+}
+
+switch (locals.brand.name) {
+  case 'Cards Against Humanity':
+    locals.brand.logo = 'cahlogo.svg';
+    break;
+  default:
+    locals.brand.logo = 'counterfeitlogo.svg';
+    break;
+}
+
 fs.writeFileSync('index.html',renderer(locals),'utf8');
