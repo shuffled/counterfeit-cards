2ca36868484eabbfb27b7648f55e1b0296cce0ac
Stuart P. Bentley <stuart@testtrack4.com>
2016-11-07 10:13:47 +0000

    Add registered-trademark superscripting
    TODO: Make this optional/clientside, include "TM"

--- render.js
+++ render.js
@@ -14,6 +14,20 @@ if (!locals.brand.name) {
   locals.brand.name = 'Counterfeit Cards';
 }
 
+let superscript = ['&reg;','®'];
+let supRegExp = new RegExp('('+superscript.join('|')+')','g');
+
+function adjustCards(cards) {
+  for (let card of cards) {
+    card.text = card.text.replace(supRegExp,'<sup>$1</sup>');
+  }
+}
+
+for (let cardSet of locals.sets) {
+  adjustCards(cardSet.cards.white);
+  adjustCards(cardSet.cards.black);
+}
+
 switch (locals.brand.name) {
   case 'Cards Against Humanity':
     locals.brand.logo = 'cahlogo.svg';
